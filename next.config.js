437c60320cc846836b9ae7646fb473b0bd2a72cc
HLDDian <a59715a@yahoo.com.tw>
2025-03-05 14:49:36 +0800

    改為靜態輸出

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export', // 讓 Next.js 以靜態 HTML 輸出
+    images: {
+        unoptimized: true, // 避免 Next.js 圖片最佳化問題
+    },
+}
 
 module.exports = nextConfig
